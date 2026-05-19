7adfc7b57712ef7c3e47b74f57d1c9da91e6ce92
Md Abdul Adud <sopnilali24@gmail.com>
2026-05-20 00:54:32 +0600

    sds

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Add your project directory to the sys.path
-project_home = '/home/YOUR_USERNAME/youtube-downloader'
+project_home = '/home/sopnilali/youtube-downloader'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
